a7824cbbbf9262e83ee44e4c6d5540c1a67d25ac
spuxx1701 <ameisenfutter@outlook.com>
2022-07-26 13:31:00 +0200

    introduced .env and db integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: process.env.DATABASE_SYNCHRONIZE.toLowerCase() === 'true',
+    }),
+  ],
 })
 export class AppModule {}
