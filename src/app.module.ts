a02131b21df4e528ad86df38a401d933b0629b75
spuxx1701 <ameisenfutter@outlook.com>
2022-08-13 18:38:13 +0200

    removed hello world route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import UsersModule from './modules/users.module';
