4c78858722c66594c73a17c1574f26d039dc34c7
Spuxx <ameisenfutter@outlook.com>
2022-09-13 11:04:40 +0200

    implemented cors with multiple origins

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { createMappings } from './mappings/mappings';
 import { ValidationPipe } from '@nestjs/common';
 import { buildSwaggerConfig } from './config/swagger.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+
+  const configService = app.get(ConfigService);
+  const origins = configService.get<string>('CORS_ALLOWED_ORIGINS').split(',');
+  app.enableCors({
+    origin: origins,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
