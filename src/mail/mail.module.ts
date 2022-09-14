dbc38e04535cdb8aa8b35600e0f3d88cb1d1d47a
Spuxx <ameisenfutter@outlook.com>
2022-09-14 15:59:09 +0200

    mail module now properly reads env

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,38 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import mailConfig from './mail.config';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
-  imports: [MailerModule.forRoot(mailConfig)],
+  imports: [
+    ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+        debug: process.env.DEBUG === 'true',
+      },
+      defaults: {
+        from: 'Doleo <noreply@doleo-2.de>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   providers: [MailService],
   exports: [MailService],
 })
