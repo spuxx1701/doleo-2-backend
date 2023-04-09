f42fa82ceeca03311aecb1d28d93f4b8a58992d2
Spuxx <ameisenfutter@outlook.com>
2023-04-09 18:39:50 +0200

    fixed validation failure for login dto

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export default class LoginDto {
   @ApiProperty({
     description: 'Your email (not your displayname!).',
   })
+  @IsString()
   username: string;
 
   @ApiProperty({
     description: 'Your password',
   })
+  @IsString()
   password: string;
 }
