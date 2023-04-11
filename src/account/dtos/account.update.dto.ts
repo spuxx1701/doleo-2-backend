bd71209e616aa88bf215881dbbfbed8911271daa
Spuxx <ameisenfutter@outlook.com>
2023-04-11 17:38:52 +0200

    added missing DTO validation decorators

--- src/account/dtos/account.update.dto.ts
+++ src/account/dtos/account.update.dto.ts
@@ -1,21 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { AutoMap } from '@automapper/classes';
 import { accountProperties } from './account.properties';
+import { IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export default class AccountUpdateDto {
   @ApiProperty(accountProperties.displayName)
   @AutoMap()
+  @IsString()
+  @IsOptional()
   displayName: string;
 
   @ApiProperty(accountProperties.selectedDesign)
   @AutoMap()
+  @IsNumber()
+  @IsOptional()
   selectedDesign: number;
 
   @ApiProperty(accountProperties.email)
   @AutoMap()
+  @IsEmail()
+  @IsOptional()
   readonly email: string;
 
   @ApiProperty(accountProperties.password)
   @AutoMap()
+  @IsString()
+  @IsOptional()
   readonly password: string;
 }
--- src/lists/dtos/list/list.update.dto.ts
+++ src/lists/dtos/list/list.update.dto.ts
@@ -1,30 +1,41 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { listProperties } from './list.properties';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export default class ListUpdateDto {
   @ApiProperty(listProperties.displayName)
   @ApiPropertyOptional()
   @AutoMap()
+  @IsString()
+  @IsOptional()
   displayName: string;
 
   @ApiProperty(listProperties.iconName)
   @ApiPropertyOptional()
   @AutoMap()
+  @IsString()
+  @IsOptional()
   iconName: string;
 
   @ApiProperty(listProperties.usesCheck)
   @ApiPropertyOptional()
   @AutoMap()
+  @IsBoolean()
+  @IsOptional()
   usesCheck: boolean;
 
   @ApiProperty(listProperties.hasAmounts)
   @ApiPropertyOptional()
   @AutoMap()
+  @IsBoolean()
+  @IsOptional()
   hasAmounts: boolean;
 
   @ApiProperty(listProperties.usesConfirmDelete)
   @ApiPropertyOptional()
   @AutoMap()
+  @IsBoolean()
+  @IsOptional()
   usesConfirmDelete: boolean;
 }
